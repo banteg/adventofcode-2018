fe93f79e6cfa59bb2f54a229e5fc48c42b517e72
banteg <banteeg@gmail.com>
2018-12-03 12:27:49 +0700

    update day 01

--- day01.py
+++ day01.py
@@ -1,22 +1,33 @@
 from itertools import cycle
-import adventofcode
+import aoc
 
-data = [
-    int(x) for x in
-    adventofcode.load_input(1).splitlines()
-]
 
-value = 0
-for adj in data:
-    value += adj
-print(value)
+@aoc.test({
+    '+1\n-2\n+3\n+1': 3,
+    '+1\n+1\n+1': 3,
+    '+1\n+1\n-2': 0,
+    '-1\n-2\n-3': -6,
+})
+def part_1(data):
+    value = 0
+    for adj in data.splitlines():
+        value += int(adj)
+    return value
 
-value = 0
-seen = set()
-for adj in cycle(data):
-    value += adj
-    if value in seen:
-        break
-    seen.add(value)
 
-print(value)
+@aoc.test({
+    '+1\n-2\n+3\n+1': 2,
+    '+1\n-1': 0,
+    '+3\n+3\n+4\n-2\n-4': 10,
+    '-6\n+3\n+8\n+5\n-6': 5,
+    '+7\n+7\n-2\n-7\n-4': 14,
+})
+def part_2(data):
+    value = 0
+    seen = {0}
+    for adj in cycle(data.splitlines()):
+        value += int(adj)
+        if value in seen:
+            break
+        seen.add(value)
+    return value
