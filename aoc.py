6e6f5c3fb1165097f4f24f41c37e58e72053e64b
banteg <banteeg@gmail.com>
2018-12-03 12:37:35 +0700

    update framework

--- aoc.py
+++ aoc.py
@@ -1,6 +1,9 @@
 import re
-import adventofcode
 import click
+import requests
+from os.path import exists
+from config import cookies
+
 
 ok = click.style('✔︎', fg='green')
 fail = click.style('✘', fg='red')
@@ -13,17 +16,37 @@ def test(cases):
         click.secho(f'day {day}, part {part}')
         tests_ok = True
         for case, expected in cases.items():
+            case_pretty = case.replace('\n', ', ')
             result = f(case)
             if result == expected:
-                click.secho(f'{ok} {repr(case)} == {result}')
+                click.secho(f'{ok} {case_pretty} == {result}')
             else:
-                print(f'{fail} {repr(case)} == {result}, expected {expected}')
+                print(f'{fail} {case_pretty} == {result}, expected {expected}')
                 tests_ok = False
         if tests_ok:
-            data = adventofcode.load_input(day)
+            data = load_input(day)
             result = f(data)
             click.secho(f'{result}\n')
         else:
             click.secho('tests failed\n', fg='red')
         return f
     return decorator
+
+
+def input_name(day):
+    return f'inputs/day{day:02d}.txt'
+
+
+def download_input(day, year=2018):
+    r = requests.get(f'http://adventofcode.com/{year}/day/{day}/input', cookies=cookies)
+    r.raise_for_status()
+    with open(input_name(day), 'w') as f:
+        f.write(r.text)
+    print(f'downloaded input for day {day}')
+
+
+def load_input(day):
+    name = input_name(day)
+    if not exists(name):
+        download_input(day)
+    return open(name).read().strip()
