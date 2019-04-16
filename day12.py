718e65425c29b3c8b3b12254e29fe011b1a82f26
banteg <banteeg@gmail.com>
2019-04-17 04:18:56 +0700

    add day 12 part 2

--- day12.py
+++ day12.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from itertools import count
+
 import aoc
 
 
@@ -19,22 +22,43 @@ example = '''initial state: #..#.#..##......###...###
 ####. => #'''
 
 
+def print_state(state):
+    pretty = ''.join(state.get(x, '.') for x in range(min(state), max(state) + 1))
+    print(min(state), pretty, max(state))
+
 def grow(state, notes):
-    new = {x: '.' for x in state}
+    new = defaultdict(lambda: '.')
     for note in notes:
         rule, result = note.split(' => ')
-        for i in range(min(state) + 2, max(state) - 2):
-            pots = ''.join(state[x] for x in range(i-2, i+3))
+        for i in range(min(state) - 2, max(state) + 3):
+            pots = ''.join(state.get(x, '.') for x in range(i-2, i+3))
             if pots == rule:
                 new[i] = result
     return new
+
+
+def value(state):
+    return sum(x for x in state if state[x] == '#')
 
 
 @aoc.test({example: 325})
 def part_1(data: aoc.Data):
     state, _, *notes = data.splitlines()
     state = dict(enumerate(state.split(': ')[-1]))
-    state = {x: state.get(x, '.') for x in range(-40, len(state) + 42)}
     for i in range(20):
         state = grow(state, notes)
-    return sum(x for x in state if state[x] == '#')
+    return value(state)
+
+
+@aoc.test({})
+def part_1(data: aoc.Data):
+    state, _, *notes = data.splitlines()
+    state = dict(enumerate(state.split(': ')[-1]))
+    values = [value(state)]
+    for gen in count(1):
+        state = grow(state, notes)
+        values.append(value(state))
+        diffs = [v2 - v1 for v1, v2 in zip(values, values[1:])]
+        if len(values) > 10 and len(set(diffs[-10:])) == 1:
+            # found a stable point
+            return (50000000000 - gen) * diffs[-1] + values[-1]
