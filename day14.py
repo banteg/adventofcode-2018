a483f2b3d64d8a14d1c6f5f35a0742f54af734a4
banteg <banteeg@gmail.com>
2019-04-18 01:50:11 +0700

    add day 14 part 2

--- day14.py
+++ day14.py
@@ -18,3 +18,23 @@ def part_1(data: aoc.Data):
         lop = (lop + 1 + int(recipes[lop])) % len(recipes)
         if len(recipes) > (num + 10):
             return recipes[num:num + 10]
+
+
+@aoc.test({
+    '51589': 9,
+    '01245': 5,
+    '92510': 18,
+    '59414': 2018,
+})
+def part_2(data: aoc.Data):
+    data = data.rstrip()
+    window = len(data) + 1
+    mop, lop = 0, 1
+    recipes = '37'
+    while True:
+        combined = int(recipes[mop]) + int(recipes[lop])
+        recipes += str(combined)
+        mop = (mop + 1 + int(recipes[mop])) % len(recipes)
+        lop = (lop + 1 + int(recipes[lop])) % len(recipes)
+        if data in recipes[-window:]:
+            return recipes.index(data)
