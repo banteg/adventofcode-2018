22254ff312e9a37e6ad23dbb48ba5432136947a5
banteg <banteeg@gmail.com>
2019-04-25 05:50:33 +0700

    simplify day 16

--- day16.py
+++ day16.py
@@ -3,107 +3,84 @@ from collections import defaultdict, Counter
 import aoc
 
 
-class WristDevice:
-    opcodes = [
-        'addi', 'addr',  # addition
-        'muli', 'mulr',  # multiplication
-        'bani', 'banr',  # bitwise and
-        'bori', 'borr',  # bitwise or
-        'seti', 'setr',  # assignment
-        'grti', 'gtir', 'gtrr',  # greater than
-        'eqir', 'eqri', 'eqrr',  # equality
-    ]
+def addr(r, a, b): return r[a] + r[b]
+def addi(r, a, b): return r[a] + b
 
-    def test(self, before, code, after):
-        '''find opcodes matching a sample'''
-        found = []
-        for opcode in self.opcodes:
-            registers = before.copy()
-            i, a, b, c = code
-            getattr(self, opcode)(registers, a, b, c)
-            if registers == after:
-                found.append(opcode)
-        return found
+def mulr(r, a, b): return r[a] * r[b]
+def muli(r, a, b): return r[a] * b
 
-    def recover_opcodes(self, tests):
-        '''recover opcode numbering'''
-        subtests = defaultdict(list)
-        for before, code, after in tests:
-            subtests[code[0]].append((before, code, after))
-        counts = defaultdict(Counter)
-        for i in subtests:
-            for test in subtests[i]:
-                counts[i].update(self.test(*test))
-        recovered = {}
-        while len(recovered) < len(subtests):
-            for i in counts:
-                ok = [op for op in counts[i] if op not in recovered and counts[i][op] == len(subtests[i])]
-                if len(ok) == 1:
-                    recovered[ok[0]] = i
-        return {i: op for op, i in recovered.items()}
+def banr(r, a, b): return r[a] & r[b]
+def bani(r, a, b): return r[a] & b
 
-    def run_program(self, program, opcodes):
-        '''run a program using recovered opcodes'''
-        registers = [0, 0, 0, 0]
-        for i, a, b, c in program:
-            getattr(self, opcodes[i])(registers, a, b, c)
-        return registers
+def borr(r, a, b): return r[a] | r[b]
+def bori(r, a, b): return r[a] | b
 
-    def addr(self, r, a, b, c):
-        r[c] = r[a] + r[b]
+def setr(r, a, b): return r[a]
+def seti(r, a, b): return a
 
-    def addi(self, r, a, b, c):
-        r[c] = r[a] + b
+def gtir(r, a, b): return 1 if a > r[b] else 0
+def grti(r, a, b): return 1 if r[a] > b else 0
+def gtrr(r, a, b): return 1 if r[a] > r[b] else 0
 
-    def mulr(self, r, a, b, c):
-        r[c] = r[a] * r[b]
+def eqir(r, a, b): return 1 if a == r[b] else 0
+def eqri(r, a, b): return 1 if r[a] == b else 0
+def eqrr(r, a, b): return 1 if r[a] == r[b] else 0
 
-    def muli(self, r, a, b, c):
-        r[c] = r[a] * b
 
-    def banr(self, r, a, b, c):
-        r[c] = r[a] & r[b]
+opcodes = [
+    addr, addi,
+    mulr, muli,
+    banr, bani,
+    borr, bori,
+    setr, seti,
+    gtir, grti, gtrr,
+    eqir, eqri, eqrr,
+]
 
-    def bani(self, r, a, b, c):
-        r[c] = r[a] & b
 
-    def borr(self, r, a, b, c):
-        r[c] = r[a] | r[b]
+def looks_like(before, code, after):
+    '''find opcodes matching a sample'''
+    found = []
+    for opcode in opcodes:
+        r = before.copy()
+        i, a, b, c = code
+        r[c] = opcode(r, a, b)
+        if r == after:
+            found.append(opcode)
+    return found
 
-    def bori(self, r, a, b, c):
-        r[c] = r[a] | b
 
-    def setr(self, r, a, b, c):
-        r[c] = r[a]
+def recover_opcodes(tests):
+    '''recover opcode numbering'''
+    subtests = defaultdict(list)
+    for before, code, after in tests:
+        subtests[code[0]].append((before, code, after))
+    counts = defaultdict(Counter)
+    for i in subtests:
+        for test in subtests[i]:
+            counts[i].update(looks_like(*test))
+    recovered = {}
+    while len(recovered) < len(subtests):
+        for i in counts:
+            ok = [op for op in counts[i] if op not in recovered and counts[i][op] == len(subtests[i])]
+            if len(ok) == 1:
+                recovered[ok[0]] = i
+    return {i: op for op, i in recovered.items()}
 
-    def seti(self, r, a, b, c):
-        r[c] = a
 
-    def gtir(self, r, a, b, c):
-        r[c] = 1 if a > r[b] else 0
-
-    def grti(self, r, a, b, c):
-        r[c] = 1 if r[a] > b else 0
-
-    def gtrr(self, r, a, b, c):
-        r[c] = 1 if r[a] > r[b] else 0
-
-    def eqir(self, r, a, b, c):
-        r[c] = 1 if a == r[b] else 0
-
-    def eqri(self, r, a, b, c):
-        r[c] = 1 if r[a] == b else 0
-
-    def eqrr(self, r, a, b, c):
-        r[c] = 1 if r[a] == r[b] else 0
+def run_program(program, codes):
+    '''run a program using recovered opcodes'''
+    r = [0, 0, 0, 0]
+    for i, a, b, c in program:
+        r[c] = codes[i](r, a, b)
+    return r
 
 
 @aoc.test({})
 def part_1(data: aoc.Data):
     first = data.split('\n\n\n\n')[0]
     tests = [aoc.Data(x).ints_lines for x in first.split('\n\n')]
-    device = WristDevice()
-    return sum(len(device.test(*test)) >= 3 for test in tests)
+    return sum(len(looks_like(*test)) >= 3 for test in tests)
 
 
 @aoc.test({})
@@ -111,7 +88,6 @@ def part_2(data: aoc.Data):
     first, second = data.split('\n\n\n\n')
     tests = [aoc.Data(x).ints_lines for x in first.split('\n\n')]
     program = aoc.Data(second).ints_lines
-    device = WristDevice()
-    opcodes = device.recover_opcodes(tests)
-    registers = device.run_program(program, opcodes)
+    opcodes = recover_opcodes(tests)
+    registers = run_program(program, opcodes)
     return registers[0]
