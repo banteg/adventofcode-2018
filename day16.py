c0521e499a8da300a719c86ca691bad58009d55a
banteg <banteeg@gmail.com>
2019-04-25 04:06:24 +0700

    add day 16 part 2

--- day16.py
+++ day16.py
@@ -1,3 +1,5 @@
+from collections import defaultdict, Counter
+
 import aoc
 
 
@@ -13,6 +15,7 @@ class WristDevice:
     ]
 
     def test(self, before, code, after):
+        '''find opcodes matching a sample'''
         found = []
         for opcode in self.opcodes:
             registers = before.copy()
@@ -21,6 +24,30 @@ class WristDevice:
             if registers == after:
                 found.append(opcode)
         return found
+
+    def recover_opcodes(self, tests):
+        '''recover opcode numbering'''
+        subtests = defaultdict(list)
+        for before, code, after in tests:
+            subtests[code[0]].append((before, code, after))
+        counts = defaultdict(Counter)
+        for i in subtests:
+            for test in subtests[i]:
+                counts[i].update(self.test(*test))
+        recovered = {}
+        while len(recovered) < len(subtests):
+            for i in counts:
+                ok = [op for op in counts[i] if op not in recovered and counts[i][op] == len(subtests[i])]
+                if len(ok) == 1:
+                    recovered[ok[0]] = i
+        return {i: op for op, i in recovered.items()}
+
+    def run_program(self, program, opcodes):
+        '''run a program using recovered opcodes'''
+        registers = [0, 0, 0, 0]
+        for i, a, b, c in program:
+            getattr(self, opcodes[i])(registers, a, b, c)
+        return registers
 
     def addr(self, r, a, b, c):
         r[c] = r[a] + r[b]
@@ -73,7 +100,18 @@ class WristDevice:
 
 @aoc.test({})
 def part_1(data: aoc.Data):
-    first = data.split('\n\n\n')[0]
+    first = data.split('\n\n\n\n')[0]
     tests = [aoc.Data(x).ints_lines for x in first.split('\n\n')]
-    vm = WristDevice()
-    return sum(len(vm.test(*test)) >= 3 for test in tests)
+    device = WristDevice()
+    return sum(len(device.test(*test)) >= 3 for test in tests)
+
+
+@aoc.test({})
+def part_2(data: aoc.Data):
+    first, second = data.split('\n\n\n\n')
+    tests = [aoc.Data(x).ints_lines for x in first.split('\n\n')]
+    program = aoc.Data(second).ints_lines
+    device = WristDevice()
+    opcodes = device.recover_opcodes(tests)
+    registers = device.run_program(program, opcodes)
+    return registers[0]
