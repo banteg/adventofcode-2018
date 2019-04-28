ce8aab18d9b97889a3fb63147f91833a33ee35c9
banteg <banteeg@gmail.com>
2019-04-29 01:48:23 +0700

    add day 19 part 2

--- day19.py
+++ day19.py
@@ -1,5 +1,17 @@
 import aoc
 
+
+examples = {
+    '''#ip 0
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5''': 6
+}
+
 codes = {
     'addr': lambda r, a, b: r[a] + r[b],
     'addi': lambda r, a, b: r[a] + b,
@@ -22,35 +34,49 @@ codes = {
 
 def run_program(code):
     r = [0, 0, 0, 0, 0, 0]
-    ip, *program = code.splitlines()
-    ip_register = int(ip.split()[1])
-    ip = 0
+    ipr, *program = code.splitlines()
+    ipr = int(ipr.split()[1])
     lines = dict(enumerate(program))
 
+    ip = 0
     while True:
-        before = r.copy()
         i, *args = lines[ip].split()
         a, b, c = [int(x) for x in args]
-        r[ip_register] = ip
+        r[ipr] = ip
+        bef = r.copy()
         r[c] = codes[i](r, a, b)
-        ip = r[ip_register] + 1
-        if ip not in lines:  # halt
+        ip = r[ipr] + 1
+        # print(f'{n} ip={ip} {bef} {lines[ip]} {r}')
+        if ip not in lines:
             return r[0]
-        # print(f'ip={ip} {before} {lines[ip]} {r}')
 
 
-examples = {
-    '''#ip 0
-seti 5 0 1
-seti 6 0 2
-addi 0 1 0
-addr 1 2 3
-setr 1 0 0
-seti 8 0 4
-seti 9 0 5''': 6
-}
+def run_optimized(code, r0):
+    r = [r0, 0, 0, 0, 0, 0]
+    ipr, *program = code.splitlines()
+    ipr = int(ipr.split()[1])
+    lines = dict(enumerate(program))
+
+    ip = 0
+    while True:
+        i, *args = lines[ip].split()
+        a, b, c = [int(x) for x in args]
+        r[ipr] = ip
+        if ip == 1:
+            divisors = [x for x in range(1, r[5] + 1) if not r[5] % x]
+            return sum(divisors)
+        else:
+            r[c] = codes[i](r, a, b)
+        ip = r[ipr] + 1
+        if ip not in lines:
+            return r[0]
 
 
 @aoc.test(examples)
 def part_1(data: aoc.Data):
     return run_program(data)
+
+
+@aoc.test({})
+def part_2(data: aoc.Data):
+    return run_optimized(data, 1)
